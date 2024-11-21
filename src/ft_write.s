2b5b676b8f8ee4ee3a380fa26f9ade14bf991875
hanmpark <hanmin.01.park@gmail.com>
2024-11-21 19:45:19 +0100

    fix: getting the right ft_write errno code

--- src/ft_write.s
+++ src/ft_write.s
@@ -3,17 +3,27 @@ section .text
 	extern __errno_location
 
 ft_write:
+	cmp rsi, 0
+	je .invalid_buffer
 	mov rax, 1
 	syscall
-	test rax, rax
-	jns .done
+	cmp rax, 0
+	jge .done
 
-	mov r8, rax
-	neg r8
-	call __errno_location wrt ..plt
-	mov [rax], r8
-	mov rax, -1
-	ret
+	.error:
+		mov r8, rax
+		neg r8
+		call __errno_location wrt ..plt
+		mov [rax], r8
+		mov rax, -1
+		ret
+
+	.invalid_buffer:
+		mov r8, 22
+		call __errno_location wrt ..plt
+		mov [rax], r8
+		mov rax, -1
+		ret
 
 	.done:
 		ret
